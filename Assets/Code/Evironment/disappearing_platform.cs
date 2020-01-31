ff67849f5ed2ab282b454a2195366e6ba0de1154
Mihael <golob.mihael@gmail.com>
2020-01-31 23:16:41 +0100

    added moving and disappearing platform script

---
+++ Assets/Code/Evironment/disappearing_platform.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Net.Sockets;
+using System.Security.Cryptography;
+using UnityEngine;
+
+public class disappearing_platform : MonoBehaviour {
+    [SerializeField] private float transitionTime;
+
+    private Renderer _renderer;
+    private bool _triggered;
+    private float _startScaleX;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        _renderer = GetComponent<Renderer>();
+        _triggered = false;
+        _startScaleX = transform.localScale.x;
+    }
+
+    private void OnTriggerEnter(Collider other) {
+        if(!_triggered)
+            StartCoroutine(disappear());
+    }
+
+    IEnumerator disappear() {
+        _triggered = true;
+        for (float i = 1f; i > 0f; i -= 0.01f) {
+            transform.localScale = new Vector3(_startScaleX*i, transform.localScale.y, transform.localScale.z);
+
+            print(i);
+
+            yield return new WaitForSeconds(transitionTime / 100.0f);
+        }
+        Destroy(gameObject);
+    }
+}
---
+++ Assets/Code/Evironment/moving_platform.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class moving_platform : MonoBehaviour {
+    
+    // positions:
+    [SerializeField] Transform startPos;
+    [SerializeField] Transform endPos;
+    private float _totalDistance;
+    
+    // speed:
+    [SerializeField] private float speed;
+
+    private float _startTime;
+    
+    
+    // Start is called before the first frame update
+    void Start() {
+        _startTime = Time.time;
+        _totalDistance = Vector3.Distance(startPos.position, endPos.position);
+        transform.position = startPos.position;
+    }
+
+    // Update is called once per frame
+    void Update() {
+        float distanceCovered = (Time.time - _startTime) * speed;
+
+        float fractionOfDistance = distanceCovered / _totalDistance;
+        transform.position = Vector3.Lerp(startPos.position, endPos.position, fractionOfDistance);
+
+        if (transform.position == endPos.position) {
+            // change direction
+            Transform tmp = endPos;
+            endPos = startPos;
+            startPos = tmp;
+            _startTime = Time.time;
+        }
+    }
+}
