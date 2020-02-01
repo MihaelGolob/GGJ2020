dbdc0b7d93f0d7389dee28db7cd64003bfc31772
Domen Golob <domen.golob@outfit7.com>
2020-02-01 23:14:20 +0100

    Locomotion disable

--- Assets/Code/Character/AnimationController.cs
+++ Assets/Code/Character/AnimationController.cs
@@ -38,6 +38,7 @@ public class AnimationController : MonoBehaviour {
     [SerializeField] private GameObject Bullets;
     [SerializeField] private Vector3 BulletOffset;
     [SerializeField] private float BulletForce;
+    [SerializeField] private bool DisableLocomotion;
     private readonly int PushHash = Animator.StringToHash("Push");
     private readonly int JumpHash = Animator.StringToHash("Jump");
     private readonly int LocomotionHash = Animator.StringToHash("Locomotion");
@@ -45,11 +46,12 @@ public class AnimationController : MonoBehaviour {
     private readonly int ShootHash = Animator.StringToHash("Shoot");
     private Animator Animator;
     private Rigidbody Rigidbody;
+    private Rigidbody LastPushObject;
     private Grounded Grounded;
     private int Locomotion = 1;
     private int Orientation = 1;
     private int LastOrientation = 1;
-    private Rigidbody LastPushObject;
+    private bool PushButtonDown = false;
 
     private void Awake() {
         Animator = GetComponent<Animator>();
@@ -59,13 +61,16 @@ public class AnimationController : MonoBehaviour {
 
     private void Update() {
         CheckGround();
-        ApplyLocomotionInput();
-        ApplyOrientation();
-        ApplyLocomotion();
-        Jump();
-        UnsetPushing();
-        MakeOneDanceMove();
-        Shoot();
+        if (!DisableLocomotion) {
+            ApplyLocomotionInput();
+            ApplyOrientation();
+            ApplyLocomotion();
+            Jump();
+            SetPushButtonDown();
+            UnsetPushing();
+            MakeOneDanceMove();
+            Shoot();
+        }
     }
 
     private void CheckGround() {
@@ -118,15 +123,10 @@ public class AnimationController : MonoBehaviour {
         Animator.SetTrigger(JumpHash);
     }
 
-    void Push(Collider other) {
-        if (Input.GetButton("Push") && other.CompareTag("Pushable")) {
-            IsPushingFlag = true;
-            Animator.SetBool(PushHash, true);
-            other.GetComponent<Rigidbody>().mass = 0.01f;
-            LastPushObject = other.GetComponent<Rigidbody>();
-        } else {
-            IsPushingFlag = false;
-            Animator.SetBool(PushHash, false);
+    private void SetPushButtonDown() {
+        PushButtonDown = false;
+        if (Input.GetButtonDown("Push")) {
+            PushButtonDown = true;
         }
     }
 
@@ -164,6 +164,18 @@ public class AnimationController : MonoBehaviour {
     private void OnTriggerExit(Collider other) {
         Animator.SetBool(PushHash, false);
         if (LastPushObject != null) LastPushObject.mass = 100_000;
+    }
+
+    void Push(Collider other) {
+        if ((PushButtonDown || Input.GetButton("Push") || Input.GetKey(KeyCode.K) || Input.GetKey(KeyCode.Mouse2)) && other.CompareTag("Pushable")) {
+            IsPushingFlag = true;
+            Animator.SetBool(PushHash, true);
+            other.GetComponent<Rigidbody>().mass = 0.01f;
+            LastPushObject = other.GetComponent<Rigidbody>();
+        } else {
+            IsPushingFlag = false;
+            Animator.SetBool(PushHash, false);
+        }
     }
 
 #endregion
