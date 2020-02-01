6af8ac00996e57ccd0f7e17b08f5fa3abd3c5ecd
Mihael <golob.mihael@gmail.com>
2020-02-01 12:01:13 +0100

    added a sample level

--- Assets/Code/Evironment/disappearing_platform.cs
+++ Assets/Code/Evironment/disappearing_platform.cs
@@ -2,22 +2,27 @@
 using System.Collections;
 using System.Collections.Generic;
 using System.Net.Sockets;
+using System.Numerics;
 using System.Security.Cryptography;
 using UnityEngine;
+using Vector3 = UnityEngine.Vector3;
 
 public class disappearing_platform : MonoBehaviour {
     [SerializeField] private float transitionTime;
+    [SerializeField] private bool xAxis;
+    [SerializeField] private bool yAxis;
+    [SerializeField] private bool zAxis;
     
     private bool _triggered;
-    private float _startScaleX;
+    private Vector3 _startScale;
     
     // Start is called before the first frame update
     void Start() {
         _triggered = false;
-        _startScaleX = transform.localScale.x;
+        _startScale = transform.localScale;
     }
 
-    private void OnTriggerEnter(Collider other) {
+    private void OnCollisionEnter(Collision other) {
         if(!_triggered)
             StartCoroutine(disappear());
     }
@@ -25,7 +30,12 @@ public class disappearing_platform : MonoBehaviour {
     IEnumerator disappear() {
         _triggered = true;
         for (float i = 1f; i > 0f; i -= 0.01f) {
-            transform.localScale = new Vector3(_startScaleX*i, transform.localScale.y, transform.localScale.z);
+            if(xAxis)
+                transform.localScale = new Vector3(_startScale.x*i, transform.localScale.y, transform.localScale.z);
+            if(yAxis)
+                transform.localScale = new Vector3(transform.localScale.x, _startScale.y*i, transform.localScale.z);
+            if(zAxis)
+                transform.localScale = new Vector3(transform.localScale.x, transform.localScale.y, _startScale.z*i);
 
             print(i);
 
--- Assets/Code/Evironment/moving_platform.cs
+++ Assets/Code/Evironment/moving_platform.cs
@@ -11,30 +11,43 @@ public class moving_platform : MonoBehaviour {
     
     // speed:
     [SerializeField] private float speed;
+    [SerializeField] private int waitTime;
 
     private float _startTime;
-    
+    private bool _waiting;
     
     // Start is called before the first frame update
     void Start() {
         _startTime = Time.time;
         _totalDistance = Vector3.Distance(startPos.position, endPos.position);
+        _waiting = false;
+        
         transform.position = startPos.position;
     }
 
     // Update is called once per frame
     void Update() {
-        float distanceCovered = (Time.time - _startTime) * speed;
+        if (!_waiting) {
+            float distanceCovered = (Time.time - _startTime) * speed;
 
-        float fractionOfDistance = distanceCovered / _totalDistance;
-        transform.position = Vector3.Lerp(startPos.position, endPos.position, fractionOfDistance);
-
-        if (transform.position == endPos.position) {
-            // change direction
-            Transform tmp = endPos;
-            endPos = startPos;
-            startPos = tmp;
-            _startTime = Time.time;
+            float fractionOfDistance = distanceCovered / _totalDistance;
+            transform.position = Vector3.Lerp(startPos.position, endPos.position, fractionOfDistance);   
         }
+
+        if (transform.position == endPos.position && !_waiting) {
+            StartCoroutine(wait());
+        }
+    }
+
+    IEnumerator wait() {
+        _waiting = true;
+        yield return new WaitForSeconds(waitTime);
+        // change direction
+        Transform tmp = endPos;
+        endPos = startPos;
+        startPos = tmp;
+        _startTime = Time.time;
+        
+        _waiting = false;
     }
 }
