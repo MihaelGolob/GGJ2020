f59bec4501481b65601c52ce53612bbc7fff1890
Mihael <golob.mihael@gmail.com>
2020-02-01 13:23:47 +0100

    prefabs for level design and new enemy script

---
+++ Assets/Enemy/EnemyMovement.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class EnemyMovement : MonoBehaviour {
+    private CharacterController _controller;
+    private Animator _animator;
+
+    private void Start() {
+        _controller = GetComponent<CharacterController>();
+        _animator = GetComponent<Animator>();
+    }
+
+    private void Update() {
+        move();
+    }
+
+    private void move() {
+        Vector2 Movement = new Vector2(1, 0);
+        
+    }
+}
